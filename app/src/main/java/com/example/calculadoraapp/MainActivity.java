6a1244a0991df27b71deb911ae7b72bca024d356
Jaime Janer <J.janer1998@gmail.com>
2024-02-10 18:28:53 -0500

    Primer commit en local.

---
+++ app/src/main/java/com/example/calculadoraapp/MainActivity.java
@@ -0,0 +1,240 @@
+package com.example.calculadoraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnSumar, btnMultiplicar, btnDividir, btnRestar, btnIgual;
+    private TextView txtResultado;
+    private TextView txtEdit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnSumar= findViewById(R.id.btnSumar);
+        btnRestar= findViewById(R.id.btnRestar);
+        btnDividir= findViewById(R.id.btnDividir);
+        btnMultiplicar= findViewById(R.id.btnMultiplicar);
+        txtResultado = findViewById(R.id.txtResultado);
+        txtEdit = findViewById(R.id.txtEdit);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("0");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "0");
+                }
+
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("1");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "1");
+                }
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("2");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "2");
+                }
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("3");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "3");
+                }
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("4");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "4");
+                }
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("5");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "5");
+                }
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("6");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "6");
+                }
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("7");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "7");
+                }
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("8");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "8");
+                }
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+                    txtEdit.setText("9");
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "9");
+                }
+            }
+        });
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "+");
+                }
+            }
+        });
+
+        btnMultiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "*");
+                }
+            }
+        });
+
+        btnRestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "-");
+                }
+            }
+        });
+
+        btnDividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtEdit.getText().toString().equals("0"))
+                {
+
+                }
+                else
+                {
+                    txtEdit.setText(txtEdit.getText().toString() + "/");
+                }
+            }
+        });
+
+    }
+
+
+}
